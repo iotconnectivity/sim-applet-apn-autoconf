9c81eff70571355022cde824c526170b40a1d6eb
Diego Leñero Ramírez <dielenram@gmail.com>
2018-10-16 18:43:49 +0200

    First version of APN autoconf

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # sim-applet-apn-autoconf
+
 JavaCard SIM card applet to configure a PDP Context with a correct APN via RUN AT proactive command
+
+
+## Table of Contents
+
+- [Description](#description)
+- [To Do](#todo)
+- [Installation and usage](#installation)
+- [Contributing and license](#contributing)
+
+
+## Description
+
+Modems are configured by AT commands. The card application toolkit standard has a mechanism to send AT COMMANDS from the SIM card. (see ["RUN AT COMMAND" ETSI TS 102.223] (https://www.etsi.org/deliver/etsi_ts/102200_102299/102223/12.01.00_60/ts_102223v120100p.pdf). 
+
+The purpose of this applet is to configure an APN by sending AT commands from the SIM card.
+
+
+## ToDo
+
+- To find a device supports this feature and test it.
+
+## Installation
+
+To install the applet you need:
+- sysmoUSIM-SJS1 SIM + USIM Card with ADM1 key (find it here: http://shop.sysmocom.de/products/sysmousim-sjs1).
+- a card reader.
+- The scripts for installation are in the osmocom [repository] (http://git.osmocom.org/sim/sim-tools/) and you have a guide [here](https://osmocom.org/projects/sim-toolkit/wiki)
+
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/fraction/readme-boilerplate/compare/).
+
+Please note this source code has been released under the GPLv3 terms and all contributions will be considered. Have a look at the LICENSE file distributed with this code.
+
---
+++ src/APNautoconf/APNautoconf.java
@@ -0,0 +1,84 @@
+/**
+ *  APN autoconfigurarion applet
+ *
+ * 		Author: Diego Leñero Ramírez 
+ *				diego.lenero@podgroup.com
+ *		 
+ */
+
+/*
+*		This applet has been tested with mkr gsm 1400 hardware with unconclusive results.
+*	
+*/
+
+
+package APNautoconf;
+
+import sim.toolkit.*;
+import sim.access.*;
+import javacard.framework.*;
+
+public class APNautoconf extends Applet implements ToolkitInterface, ToolkitConstants {
+
+
+	static byte[] cgdcont = new byte[] {'a','t','+','c','g','d','c','o','n','t','=',
+										'1',','
+										,'"','I','P','"',','
+										,'"','d','a','t','a','6','4','1','0','0','3','"'};							
+
+	private ToolkitRegistry reg;
+
+
+	static 	byte[] 	menuItemText = new byte[] {'S','E','N','D',' ','A','T',' ','C','O','M','M','A','N','D'};
+	private byte 	menuItem;
+
+
+ 	/**
+ 	*	Constructor of the applet
+	*/
+	private APNautoconf() {
+		reg = ToolkitRegistry.getEntry();							// register to the SIM Toolkit Framework
+
+		// Define the applet Menu Entry
+		menuItem = reg.initMenuEntry(menuItemText, (short)0, (short)menuItemText.length,
+				PRO_CMD_SELECT_ITEM, false, (byte)0, (short)0);
+
+	}
+
+    /**
+    * Method called by the JCRE at the installation of the applet
+    */
+	public static void install(byte[] bArray, short bOffset, byte bLength) {
+		APNautoconf applet = new APNautoconf();
+		applet.register();
+	}
+
+
+	public void process(APDU apdu) throws ISOException {
+		if (selectingApplet())
+		return;
+	}
+	
+    /**
+    * Method called by the SIM Toolkit Framework
+    */
+	public void processToolkit(byte event) throws ToolkitException {
+
+
+
+		EnvelopeHandler envHdlr = EnvelopeHandler.getTheHandler();
+
+		if (event == EVENT_MENU_SELECTION) {
+			byte selectedItemId = envHdlr.getItemIdentifier();
+
+			if (selectedItemId == menuItem) {
+				
+				ProactiveHandler proHdlr = ProactiveHandler.getTheHandler();
+				proHdlr.init(PRO_CMD_RUN_AT_COMMAND,(byte)0,DEV_ID_ME);
+				proHdlr.appendTLV((byte)(TAG_AT_COMMAND | TAG_SET_CR), cgdcont, (short)0, (short)cgdcont.length);
+				proHdlr.send();
+			}
+		}
+	
+	}
+}
